234e05442452dc0e0bcc451fcf8c28465bfd5f3d
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-01-15 19:10:24 +0900

    boss3を進行

--- Boss3.cpp
+++ Boss3.cpp
@@ -1,17 +1,6 @@
 #include "Boss3.h"
 
-void BossAtackRotatet(Boss3 &a, Vector2 &player) {
+void BossAtackRotatet(Boss3 a, Vector2 player) {
 
 }
-void BossBariaOpen(Baria& a,Vector2 &bullet,int count,bool frag) {
-	if (a.isAlive == true) {
-		if (CircleCollisinHit(a.pos, a.size, bullet, 32/*バレットサイズ*/) == true) {
-			a.HP -= 1;
-			
-		}
-		if (a.HP <= 0) {
-			a.isAlive = false;
-	}
-}
-	
-}
+
--- Boss3.h
+++ Boss3.h
@@ -1,29 +1,16 @@
 #pragma once
 
 #include "collisionfunc.h"
-#include "matrix.h"
 
-typedef struct Baria {
-	Vector2 pos;
-	int HP;
-	bool isAlive;
-	int openCount;
-	int hitCount;
-	float size;
-	bool hitFlag;
-};
 typedef struct Boss3 {
 	Vector2 Pos;
 	Vector2 radius;
-
+	Vector2 Beem;
 	Vector2 leftTop;
 	Vector2 rightTop;
 	Vector2 leftDown;
 	Vector2 rightDown;
-	Baria gard;
+
+	
 };
-
-
-void BossAtackRotate(Boss3& a, Vector2& player);
-
-void BossBariaOpen(Baria& a, Vector2& bullet, int count, bool frag);
+void BossAtackRotate();
