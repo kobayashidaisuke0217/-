4c77a740c2776c582695fec54659c9939d53ae72
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-01-17 11:02:55 +0900

    ボス３を進めた

--- Boss3.h
+++ Boss3.h
@@ -1,23 +1,39 @@
 #pragma once
 
 #include "collisionfunc.h"
-
+#include "matrix.h"
 typedef struct Baria {
 	Vector2 pos;
-	bool isAlive;
-	bool Flag;
-};
-typedef struct Boss3 {
-	Vector2 Pos;
-	Vector2 radius;
-	Vector2 Beem;
 	Vector2 leftTop;
 	Vector2 rightTop;
 	Vector2 leftDown;
 	Vector2 rightDown;
-	Baria baria;
+	float Radius;
+	bool isAlive;
+	bool Flag;
+	int count;
+	int alpha;
+	int breakCount;
+	int HP;
+};
+typedef struct Boss3 {
+	Vector2 Pos;
+	float radius;
+	Vector2 leftTop;
+	Vector2 rightTop;
+	Vector2 leftDown;
+	Vector2 rightDown;
+	int HP;
+	int isAlive;
+	int patten;
+	enum choice {
+		rotate,
+		baria,
+
+	};
 	
 };
-
+void Boss3Rest();
 void BossAtackRotate();
 void BossBaria();
+void BossPattern();
