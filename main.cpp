0b758ba9e72e90b9c759e4ec2e23393e0d3c3ed1
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-02-02 13:00:47 +0900

    画像の追加

--- main.cpp
+++ main.cpp
@@ -228,6 +228,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	 int Clearpic= Novice::LoadTexture("./Resources/image/Gameclear.png");
 	 int hpBar1= Novice::LoadTexture("./Resources/image/HP1.png");
 	 int hpBar2= Novice::LoadTexture("./Resources/image/HP2.png");
+	 int PowerUP=Novice::LoadTexture("./Resources/image/.Power1.png");
 	Vector2 Start[4];
 	Vector2 Vertex[4];
 	Vector2 End[4];
@@ -525,11 +526,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			for (int i = 0; i < 2; i++) {
 				Boss3Reset(lastboss, rasBossBaria, bossBeam[i]);
 			}
-			lastboss.HP = 1;
+			lastboss.HP = 25;
 			if (fadeOutAlpha >= 255) {
 				triangle->pattern = 0;
 				gamemode = 3;
-				lastboss.HP = 25;
+				
 			}
 			lastboss.battleStart = false;
 		}
@@ -913,7 +914,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 							if (nucleusSuctionCount >= 4) {
 								nucleusSuctionCount = 4;
 							}
-							//atackdamage = nucleusSuctionCount;
+							atackdamage = nucleusSuctionCount;
 							nucleus[i]->nucleusCountfrag = true;
 						}
 					}
@@ -1365,6 +1366,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 					Novice::DrawEllipse(throwPos[i].x - player->scroll.x + RandShake.x, throwPos[i].y - player->scroll.y + RandShake.y, hitradius[i], hitradius[i], 0, RED, kFillModeWireFrame);
 				}
 			}
+			if (nucleusSuctionCount > 1) {
+				Novice::DrawSprite(player->player.center.x - player->scroll.x, player->player.center.y - 30 - player->scroll.y, PowerUP, 1, 1, 0, WHITE);
+			}
 			for (int i = 0; i < bulletNum; i++) {//弾
 				if (bullet[i]->bulletOnFlag == true) {
 					Novice::DrawSprite(bullet[i]->bullet.center.x - player->scroll.x + RandShake.x - bullet[i]->bullet.radius, bullet[i]->bullet.center.y - player->scroll.y + RandShake.y - bullet[i]->bullet.radius, BulletPic, 1, 1, 0, WHITE);
@@ -1489,10 +1493,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			Novice::DrawSprite(0, 0, gameoverPic, 1, 1, 0, WHITE);
 		}
 		if (gamemode == 10) {
-			Novice::ScreenPrintf(40, 100, "gameClear");
+			
 			Novice::DrawSprite(0, 0, Clearpic, 1, 1, 0, WHITE);
 		}
-		Novice::ScreenPrintf(10, 40, "%d", gamemode);
+		Novice::ScreenPrintf(10, 40, "%d", lastboss.HP);
 
 		///
 		///
